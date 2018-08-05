0cf9912abf27b59a205b6e9909436684ebfe213d
cetiniz <zcetinic@quartermaster.house>
2018-08-05 00:15:22 -0400

    [Data Collection] Add - Added csv generator to speed up tweet labeling

---
+++ tweet_to_csv.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+from twitter_scraper import get_tweets
+
+if len(sys.argv) >= 3:
+    twitter_handle = sys.argv[1]
+    twitter_pages = sys.argv[2]
+else:
+    print("Two command line arguments are expected")
+    sys.exit()
+
+tweets = get_tweets(twitter_handle, pages=int(twitter_pages))
+
+with open('tweet_data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ', quotechar='|')
+    writer.writerow(['handle', 'text'])
+    for tweet in tweets:
+        writer.writerow([twitter_handle, tweet['text']])
