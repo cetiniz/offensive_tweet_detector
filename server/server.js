7593c7109a61de8046373c0eee59ddc42ddc21bf
cetiniz <zcetinic@quartermaster.house>
2018-08-11 15:25:16 -0400

    Started translating python tweet function into javascript

--- server/server.js
+++ server/server.js
@@ -3,9 +3,22 @@ const mongoose = require('mongoose');
 const keys = require('./keys/authkeys');
 
 const dev_uri = `mongodb://${keys.mongoose.user}:${keys.mongoose.pass}@ds119422.mlab.com:19422/twitter_ml_dev`;
+
 mongoose.connect(dev_uri, {useNewUrlParser: true}, function(err) {
   if (err) throw err;
   console.log('Successfully Connected');
+});
+
+const tweetSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  text: String,
+  author: String,
+});
+
+const labelSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  text: String,
+  author: String,
 });
 
 const app = express();
@@ -19,12 +32,8 @@ app.get('/tweet', (req, res) => {
 
 // Route to post label on tweet
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/tweet/:tweetId', (req, res) => {
+  res.send({ express: `Your tweet was ${req.params.tweetId}` });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
