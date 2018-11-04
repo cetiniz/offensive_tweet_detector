bd6488f93369bef630d1f7220a26d097917f78ac
cetiniz <zcetinic@quartermaster.house>
2018-11-04 17:01:20 -0500

    [UI] - Added twitter handle and removed profiles that have been deleted and CSV files

--- twitter_ml/src/containers/App.js
+++ twitter_ml/src/containers/App.js
@@ -35,26 +35,43 @@ class App extends Component {
       this.state = {
         tweetData: null,
         userNames: [
-          'realDonaldTrump'
+          'realDonaldTrump',
+          'OMAROSA',
+          'RichardBSpencer',
+          'seanhannity',
+          'warcryofaYeehaw',
+          'Thetaelizabeth',
+          'noriacatherine',
+          'adamlingo',
+          'OffensiveT',
+          'OffensiveTweet_',
+          'T_PartyTrucking',
+          'ronnyravegan',
+          'proletariatitty',
+          'SketchesbyBoze',
         ],
         currentTweet: 0,
         isLoading: true,
+        currentUserIndex: null,
       }
       this.fetchTweets = this.fetchTweets.bind(this);
       this.fetchNextTweet = this.fetchNextTweet.bind(this);
       this.labelTweet = this.labelTweet.bind(this);
+      this.getRandomUser = this.getRandomUser.bind(this);
     }
+
     componentDidMount() {
       this.fetchTweets();
     }
+
     async fetchTweets() {
-      const { userNames } = this.state;
-      const randomUserIndex = Math.floor(Math.random() * userNames.length);
+      const { userNames, tweetData, currentUserIndex } = this.state;
+      const randomUserIndex = currentUserIndex ? currentUserIndex : Math.floor(Math.random() * userNames.length);
+      console.log(randomUserIndex)
       await this.setState({ isLoading: true });
-      const tweetResponse = await axios.get(`/tweet/${userNames[randomUserIndex]}`);
-      console.log(tweetResponse)
+      const tweetResponse = await axios.get(`/tweet/${userNames[randomUserIndex]}/${tweetData ? tweetData[tweetData.length - 1].tweet_id : 0}`);
       if (tweetResponse.data) {
-        this.setState({ tweetData: tweetResponse.data.express, currentTweet: 0, isLoading: false });
+        this.setState({ tweetData: tweetResponse.data.express, currentTweet: 0, isLoading: false, currentUserIndex });
         console.log(tweetResponse.data.express[0].tweet_text)
       }
     }
@@ -82,40 +99,53 @@ class App extends Component {
       this.fetchNextTweet();
     }
 
+    async getRandomUser() {
+      const { userNames } = this.state;
+      await this.setState({ isLoading: true });
+      await this.setState({ currentUserIndex: Math.floor(Math.random() * userNames.length), tweetData: null  });
+      this.fetchTweets();
+    }
+
     render() {
         return (
-          <div style={{ 
-              display: 'flex', 
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100vh'
-            }}
-          >
+          <div>
+          <p>WouldntFire: Things here do not include any swear words, racism, contraversy, politics or ad hominim attacks</p>
+          <p>MaybeFire: Things here might include swear words, ad hominim attacks, contraversial topics that COULD be taken as offensive</p>
+          <p>FireAway: Things here would include directly racist things, sexism, very offensive that would be taken as offsensive to anyone</p>
             <div style={{ 
-              display: 'flex', 
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            {this.state.isLoading ? 
-              <object id="ripple" data="./assets/Ripple.svg" type="image/svg+xml"></object> : (
-               <div>
-                 <div style={tweetStyle}>
-                   <p>
-                     {this.state.tweetData && this.state.tweetData[this.state.currentTweet].tweet_text}
-                     }
-                   </p>
+                display: 'flex', 
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+              }}
+            >
+              <div style={{ 
+                display: 'flex', 
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              {this.state.isLoading ? 
+                <object id="ripple" data="./assets/Ripple.svg" type="image/svg+xml"></object> : (
+                  <div>
+                    {this.state.tweetData && this.state.tweetData[this.state.currentTweet] && this.state.tweetData[this.state.currentTweet].tweet_handle}
+                   <div style={tweetStyle}>
+                     <p>
+                       {this.state.tweetData && this.state.tweetData[this.state.currentTweet] && this.state.tweetData[this.state.currentTweet].tweet_text}
+                     </p>
+                   </div>
+                   <div>
+                    <button onClick={() => this.labelTweet(0)} style={buttonStyle.nFire}>Wouldn't Fire</button>
+                     <button onClick={() => this.labelTweet(1)} style={buttonStyle.mFire}>Maybe Fire</button>
+                     <button onClick={() => this.labelTweet(2)} style={buttonStyle.fFire}>Fire away</button>
+                   </div>
+                   <button onClick={this.getRandomUser} style={{ width: '100%', height: '100px' }}>Get new Random user!</button>
                  </div>
-                 <div>
-                   <button onClick={() => this.labelTweet(0)} style={buttonStyle.nFire}>Wouldn't Fire</button>
-                   <button onClick={() => this.labelTweet(1)} style={buttonStyle.mFire}>Maybe Fire</button>
-                   <button onClick={() => this.labelTweet(2)} style={buttonStyle.fFire}>Fire away</button>
-                 </div>
-               </div>
-              )}
-            </div>
-        </div>
+                )}
+              </div>
+          </div>
+          </div>
         );
     }
 }
